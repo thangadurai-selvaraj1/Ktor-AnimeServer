8ededf6cff5965a9bc67c951a438d60154e080c6
thangaduraiselvaraj <thangadurai.selvaraj@tatadigital.com>
2023-01-08 18:29:06 +0530

    api testing added

--- README.md
+++ README.md
@@ -16,5 +16,4 @@
     
 
 ## Todo
-    Testing
     Database Integrations
--- src/test/kotlin/com/anime/ApplicationTest.kt
+++ src/test/kotlin/com/anime/ApplicationTest.kt
@@ -1,29 +1,137 @@
 package com.anime
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
+import com.anime.models.ApiResponse
+import com.anime.models.ErrorResponse
+import com.anime.plugins.configureRouting
+import com.anime.utils.constants.RouteEndPoints
+import com.anime.utils.*
+import com.anime.utils.constants.Constants
+import com.anime.utils.extensions.calculatePage
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.anime.plugins.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+
     @Test
-    fun testRoot() = testApplication {
+    fun `access root endpoint, assert correct information`() = testApplication {
         application {
             configureRouting()
         }
-        client.get("/").apply {
+        client.get(RouteEndPoints.ROOT).apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("Hello, Welcome to My Api", bodyAsText())
         }
     }
+
+    @Test
+    fun `access all heroes endpoint, assert correct information`() = testApplication {
+        application {
+            configureRouting()
+        }
+
+        val list = listOf(page1, page2, page3, page4, page5)
+        (1..5).forEach {
+            client.get("${RouteEndPoints.ANIME_HEROES}?page=$it").apply {
+                assertEquals(HttpStatusCode.OK, status)
+
+                val expected = ApiResponse(
+                    success = true,
+                    message = "Fetched",
+                    nextPage = it.calculatePage()[Constants.NEXT_PAGE_KEY],
+                    prePage = it.calculatePage()[Constants.PRE_PAGE_KEY],
+                    heroes = list[it - 1]
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+                assertEquals(expected, actual)
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, assert invalid param information`() = testApplication {
+        application {
+            configureRouting()
+        }
+
+            client.get("${RouteEndPoints.ANIME_HEROES}?page=6").apply {
+                assertEquals(HttpStatusCode.BadRequest, status)
+
+                val expected = ApiResponse(
+                    success = false,
+                    message = "Invalid Param",
+                    heroes = emptyList()
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+                assertEquals(expected, actual)
+            }
+    }
+
+
+    @Test
+    fun `access search heroes endpoint, assert correct information`() = testApplication {
+        application {
+            configureRouting()
+        }
+
+        client.get("${RouteEndPoints.SEARCH_HEROES}?name=Boruto").apply {
+            assertEquals(HttpStatusCode.OK, status)
+
+            val expected = ApiResponse(
+                success = true,
+                message = "Fetched",
+                heroes = listOf(page2.first())
+            )
+            val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `access search heroes endpoint, assert wrong search name information`() = testApplication {
+        application {
+            configureRouting()
+        }
+
+        client.get("${RouteEndPoints.SEARCH_HEROES}?name=pipoii").apply {
+            assertEquals(HttpStatusCode.OK, status)
+
+            val expected =
+            ApiResponse(success = true, message = "Fetched")
+            val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `access invalid endpoint, assert true`() = testApplication {
+        application {
+            configureRouting()
+        }
+
+        client.get("invalid").apply {
+            val expected = ErrorResponse(
+                message = "Not Found",
+                code = 404,
+                success = false
+            )
+            val actual = Json.decodeFromString<ErrorResponse>(bodyAsText())
+
+            assertEquals(expected, actual)
+        }
+    }
+
+
 }
